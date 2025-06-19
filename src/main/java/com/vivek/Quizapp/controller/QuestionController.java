7a94a31be555bdf2e9ffa2ce58d6c8c8a52851d7
vivekgandharla <sonugandherla01@gmail.com>
2025-06-19 13:36:47 +0530

    CRUD operation for questions is completed

--- src/main/java/com/vivek/Quizapp/controller/QuestionController.java
+++ src/main/java/com/vivek/Quizapp/controller/QuestionController.java
@@ -2,8 +2,8 @@ package com.vivek.Quizapp.controller;
 
 import com.vivek.Quizapp.Question;
 import com.vivek.Quizapp.service.QuestionService;
-import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +15,17 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
 
     }
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
--- src/main/java/com/vivek/Quizapp/service/QuestionService.java
+++ src/main/java/com/vivek/Quizapp/service/QuestionService.java
@@ -3,35 +3,53 @@ package com.vivek.Quizapp.service;
 import com.vivek.Quizapp.Question;
 import com.vivek.Quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "Question added successfully";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
     public String updateQuestion(Question question) {
-        questionDao.save(question);
+        new ResponseEntity<>(questionDao.save(question), HttpStatus.ACCEPTED);
         return "Question updated successfully";
     }
 
-    public String deleteQuestion(Integer id) {
+    public ResponseEntity<String> deleteQuestion(Integer id) {
         questionDao.deleteById(id);
-        return "Question deleted successfully";
+        return new ResponseEntity<>("Question deleted successfully",HttpStatus.OK);
     }
 }
